c0549780320742d3c873d0426a8c2001dce1be09
John Oram <john@oram.ca>
2020-11-21 13:28:45 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-sudo apt -y remove nodejs
 
+
+#NVM
 git clone https://github.com/creationix/nvm.git ~/.nvm
 cd ~/.nvm
 git checkout `git describe --abbrev=0 --tags`
@@ -10,4 +11,8 @@ nvm install 4
 nvm use 4
 npm install npm@latest -g
 
+# CNCJS
 sudo npm install --unsafe-perm -g cncjs
+
+
+
