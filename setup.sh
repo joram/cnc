e30bc20f00f9d3624e1c2aca48793b178aa2616a
John Oram <john@oram.ca>
2020-11-21 17:44:35 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ git checkout `git describe --abbrev=0 --tags`
 cd ..
 ~/.nvm/install.sh
 ~/.nvm/nvm.sh
-nvm install 4
-nvm use 4
+nvm install v10.13.0
+nvm use v10.13.0
 npm install npm@latest -g
 
 # CNCJS
+npm install --save serialport 
+
 sudo npm install --unsafe-perm -g cncjs
 
 
