609b9748acd6a06e6f3108835cfd7851a21e1065
John Oram <john@oram.ca>
2020-11-25 20:55:33 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ cd ~/.nvm
 git checkout `git describe --abbrev=0 --tags`
 cd ..
 ~/.nvm/install.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+chmod 755 ~/.nvm/nvm.sh
 ~/.nvm/nvm.sh
 nvm install v10.13.0
 nvm use v10.13.0
